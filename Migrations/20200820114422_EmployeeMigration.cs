2d8c7da1a665e38440a93d9dbdd63c13be6dcf9f
thinesh <thineschowdaryduggempudi123@gmail.com>
2020-08-25 10:32:31 +0530

    first commit

---
+++ Migrations/20200820114422_EmployeeMigration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CRUDCORE.Migrations
+{
+    public partial class EmployeeMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmpID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    EmpCode = table.Column<string>(type: "Varchar(10)", nullable: true),
+                    Position = table.Column<string>(type: "Varchar(20)", nullable: true),
+                    OfficeLocation = table.Column<string>(type: "Varchar(20)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmpID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http2;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDCORE.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmpID { get; set; }
+        [Column(TypeName="nvarchar(50)")]
+        [Required]
+        public string Name { get; set; }
+        [Column(TypeName ="Varchar(10)")]
+        public string EmpCode { get; set; }
+        [Column (TypeName ="Varchar(20)")]
+        public string Position { get; set; }
+        [Column(TypeName = "Varchar(20)")]
+        public string OfficeLocation { get; set; }
+    }
+}
